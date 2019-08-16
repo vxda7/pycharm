9e0ddbdcca5203dd6e314f9f78522e50d43d5cab
vxda7 <vxda77@gmail.com>
2019-08-16 21:42:16 +0900

    palindrome

--- day4/palindrome2.py
+++ day4/palindrome2.py
@@ -2,50 +2,32 @@ import sys
 sys.stdin = open("input (12).txt", "r")
 
 
-# def find(table):
-#     for fix in range(100):  # 한줄 뜯어오기
-#         count = 100
-#         best = 1
-#         for m in range(98):    # 간격을 줄여가면서 돌기
-#             for where in range(101 - count, 0, -1):    # 100 - count -> 0 간격마다 돌기
-#                 row = ""
-#                 col = ""
-#                 for interval in range(count-1):  # 0 -> count
-#                     row += table[fix][where + interval]
-#                     col += table[where + interval][fix]
-#                 if row == row[::-1] or col == col[::-1]:
-#                     if best < len(row):
-#                         best = len(row)
-#                         return best
-#                     if best < len(col):
-#                         best = len(col)
-#                         return best
-#             count -= 1
-
 def find(table):
-    best = 0
-    for i in range(100):
-        for j in range(100):
-            interval = 100
-            row = ''
-            col = ''
-            for start in range(100 - interval + 1):
-                for k in range(interval):
-                    row += table[i][j+k]
-                    col += table[j+k][i]
-
+    interval = 100
+    rot = list(zip(*table))
+    for j in range(99):     # table 에서 한줄 빼오기   0 ~ 99
+        for i in range(100):  # 0 ~98
+            for start in range(100 - interval + 1):  # 0~101-interval
+                row = ''
+                col = ''
+                row = table[i][start: start + interval]
+                col = rot[i][start: start + interval]
                 if row == row[::-1] or col == col[::-1]:
                     if len(col) < len(row):
                         return len(row)
                     else:
                         return len(col)
-
-                interval -=1
+        interval -= 1
 
 
 for case in range(10):
     test_case = input()
     table = []
-    for j in range(100):
+    for get in range(100):
         table.append(input())
     print("#{} {}".format(test_case, find(table)))
+
+# a = [[1,2,3], [4,5,6], [7,8,9]]
+# print(a)
+# print(zip(a[:]))
+# print(list(zip(*a)))
